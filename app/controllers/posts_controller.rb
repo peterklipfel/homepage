ddbdf2225fd855ad6898ebac55bea12b81c76b9b
Peter Klipfel <peter@klipfel.me>
2013-08-28 22:30:46 -0600

    /capstone will show all capstone assignments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,22 @@ class PostsController < ApplicationController
     @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,autolink: true, 
                                                                 space_after_headers: true, 
                                                                 fenced_code_blocks:true)
+  end
 
+  def capstone
+    @files = find_all.sort.reverse
+    @date_post_name = []
+    @files.each do |file_name|
+      post_name = file_name.gsub(/.*?(?=-)/im, "").gsub("-", "")
+      if post_name.include? "capstone"
+        date = file_name.match(/[^-]+/).to_s
+        @date_post_name << [date, post_name]
+      end
+    end
+
+    respond_to do |format|
+      format.html # index.html.erb
+    end
   end
 
 protected
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,17 @@ Homepage::Application.routes.draw do
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
+  get 'capstone', to: 'posts#capstone'
+
   get 'blog', to: 'posts#index'
   get 'blog/:id', to: 'posts#show'
   get 'resume', to: 'resume#index'
   get "/404", :to => "errors#not_found"
   get "/500", :to => "errors#internal_error"
 
-  resource :landing_pages
+  resource :landing_pages, except: [:new, :delete, :show, :create]
   resource :projects
-  resource :errors
+  resource :errors, except: [:new, :delete, :show, :index, :create]
 
   root :to => 'landing_pages#index'
 
