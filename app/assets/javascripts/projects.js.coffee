fb4d5f335dbfb2c7dfcd4577895b364ebcb76d44
Peter Klipfel <peter@klipfel.me>
2013-10-10 21:31:23 -0600

    index page links to webgl projects

--- app/assets/javascripts/projects.js.coffee
+++ app/assets/javascripts/projects.js.coffee
@@ -8,13 +8,13 @@ $ ->
         for repo in obj.data
           date = new Date(repo.updated_at)
           $('.dump-github').append(
-            '<div class="github-repo">' +
+            '<div class="project-container">' +
               '<div class="six columns centered">' +
-                '<h4 class="github-repo-language">' + repo.language + '</h4>' +
-                '<h3 class="github-repo-name">' + repo.name + '</h3>' +
+                '<h4 class="project-language">' + repo.language + '</h4>' +
+                '<h3 class="project-name">' + repo.name + '</h3>' +
                 '</br>' +
-                '<span class="repo-description">'+repo.description+'</span> </br>' +
-                '<span class="repo-date">'+date.toDateString()+'</span>' +
-                '<a class="github-repo-url" href="' + repo.html_url + '">View on Github</a>' +
+                '<span class="project-description">'+repo.description+'</span> </br>' +
+                '<span class="project-date">'+date.toDateString()+'</span>' +
+                '<a class="project-link" href="' + repo.html_url + '">View on Github</a>' +
               '</div>'+
             "</div>")
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,36 @@
 class ProjectsController < ApplicationController
+  def index
+    @projects=[
+      { title: 'Visualizer', 
+        description: 'Pulls latest soundcloud songs.  Use arrows & pg up/pg down',
+        id: 'visualizer'
+      },
+      { title: 'Webcam Filters', 
+        description: 'Applies filters to webcam.  Use arrows',
+        id: 'webcam'
+      },
+      { title: 'Particle Engine', 
+        description: 'Makes spiral-like pattern with star gifs.  Use arrows & pg up/pg down',
+        id: 'stars'
+      },
+      { title: 'Procedurally Textured Box Cluster', 
+        description: 'Uses procedural texturing on a bunch of boxes.  Use arrows & pg up/pg down, w, a, s, d, q, e',
+        id: 'boxes'
+      },
+      { title: 'Procedurally Textured Box', 
+        description: 'Uses procedural texturing on a single box.  Use arrows & pg up/pg down, w, a, s, d, q, e',
+        id: 'procedural'
+      },
+      { title: 'Positional Shading', 
+        description: 'Colors box based on position on screen.  Use arrows & pg up/pg down, w, a, s, d, q, e',
+        id: 'box'
+      },
+      { title: 'Hello World', 
+        description: 'Just a box and a pyramid.  Use arrows & pg up/pg down',
+        id: 'basic'
+      }
+    ]
+  end
 	def show
 		render params[:id], layout: false
 	end
